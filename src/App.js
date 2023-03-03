d85e450d5abcfdb30ff8140b834524e0b9042da5
keneeeyt <cervantes.klc@gmail.com>
2023-03-02 16:12:39 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import PageRender from './PageRender';
 
 function App() {
   return (
+    <Router>
+    <input type='checkbox' id='theme' />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main max-w-[1000px] w-[100%] m-auto'>
+        <Routes>
+          <Route path="/:page" element={<PageRender />} />
+          <Route path="/:page/:id" element={<PageRender />} />
+        </Routes>
+      </div>
     </div>
+    </Router>
   );
 }
 
